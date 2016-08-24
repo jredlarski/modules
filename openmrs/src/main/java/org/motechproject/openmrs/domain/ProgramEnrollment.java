e5d9f719bccf70e8f88dfcd31c5cd8a332f1df1f
Jakub Redlarski <jredlarski@soldevelo.com>
2016-08-24 13:14:56 +0200

    MOTECH-2798: Add Bahmni Program Attributes to task data source lookup
    master branch in progress

--- openmrs/src/main/java/org/motechproject/openmrs/domain/ProgramEnrollment.java
+++ openmrs/src/main/java/org/motechproject/openmrs/domain/ProgramEnrollment.java
@@ -4,7 +4,9 @@ import com.google.gson.annotations.Expose;
 import org.apache.commons.collections.CollectionUtils;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -173,6 +175,14 @@ public class ProgramEnrollment {
 
     public void setAttributes(List<Attribute> attributes) {
         this.attributes = attributes;
+    }
+
+    public Map<String, String> getProgramAttributes() {
+        Map<String, String> programAttributes = new HashMap<>();
+        for (Attribute attribute : attributes) {
+            programAttributes.put(attribute.getAttributeType().getDisplay(), attribute.getValue());
+        }
+        return programAttributes;
     }
 
     public boolean isEnrolled() {
